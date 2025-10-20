fed2b4bad99fb197a917e58a343c273c48dd8359
Faheem V <faheem2002133@gmail.com>
2025-10-21 00:33:18 +0400

    Update signup and login screens with Google login and modular buttons

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fudddel/presentation/screens/startscreen.dart';
 void main() async{
-  WidgetsFlutterBinding.ensureInitialized(); //firebase initialization
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
